08166913830925796b91198976307615d8f9eae6
alphaeletrica <suporte@alphaeletrica.ind.br>
2025-03-27 17:09:18 -0300

    v1.1.4

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,7 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     
-    # Configuração segura para o Vercel
+    # Configuração segura para PostgreSQL
     db_url = os.getenv('DATABASE_URL', '').replace('postgres://', 'postgresql://')
     if not db_url:
         raise RuntimeError("DATABASE_URL não configurada")
